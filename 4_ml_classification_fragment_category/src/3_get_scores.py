2cc54ded8e88298a8841b08cbb61497112e82028
Deepro Banerjee <dzb5732@sarah.bx.psu.edu>
2023-06-08 18:15:14 -0400

    cross-lib-pred-start

--- 4_ml_classification_fragment_category/src/3_get_scores.py
+++ 4_ml_classification_fragment_category/src/3_get_scores.py
@@ -14,13 +14,11 @@ def get_test_scores(pred_save_file, thresh=0.5):
     f1 = f1_score(df.ytarget, (df.ypred>thresh).astype(int))
     return aps, prec, rec, acc, f1, roc
 
-def save_test_scores(proj_dir, lib, model, class1, class2):
-    save_file = os.path.join(proj_dir, lib, f"{class1}_vs_{class2}", model, f"{model}.csv.gz")
-    aps, prec, rec, acc, f1, roc = get_test_scores(save_file)
+def save_test_scores(pred_save_file, score_save_file):
+    aps, prec, rec, acc, f1, roc = get_test_scores(pred_save_file)
     # save scores on test
-    save_test_score_file = os.path.join(proj_dir, lib, f"{class1}_vs_{class2}", model, "scores.csv")
-    os.makedirs(os.path.dirname(save_test_score_file), exist_ok=True)
-    with open(save_test_score_file, "w") as f:
+    os.makedirs(os.path.dirname(score_save_file), exist_ok=True)
+    with open(score_save_file, "w") as f:
         f.write("APS,Precision,Recall,Accuracy,F1_score,ROCAUC\n")
         f.write(f"{aps},{prec},{rec},{acc},{f1},{roc}\n")
     return
@@ -28,12 +26,9 @@ def save_test_scores(proj_dir, lib, model, class1, class2):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="STARRSeq enhancer classification")
-    parser.add_argument("lib", type=str, help="library shortform")
-    parser.add_argument("model", type=str, help="the model used to classify")
-    parser.add_argument("proj_dir", type=str, help="project dir where the fragments classified by their activity is stored")
-    parser.add_argument("class1", type=str, help="Fragment Class 1")
-    parser.add_argument("class2", type=str, help="Fragment Class 2")
+    parser.add_argument("pred_filepath", type=str, help="File where model predictions are stored")
+    parser.add_argument("score_filepath", type=str, help="File where model scores will be stored")
 
     cli_args = parser.parse_args()
 
-    save_test_scores(cli_args.proj_dir, cli_args.lib, cli_args.model, cli_args.class1, cli_args.class2)
+    save_test_scores(cli_args.pred_filepath, cli_args.score_filepath)
